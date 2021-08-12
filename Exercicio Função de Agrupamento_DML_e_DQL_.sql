2256459e122ebada0645751dd6fd6d2d939b413f
Matheus Pereira <matheusrpm007@gmail.com>
2021-08-12 10:57:29 -0300

    Exercicio Função de Agrupamento Feito

--- Exercicio Função de Agrupamento_DML_e_DQL_.sql
+++ Exercicio Função de Agrupamento_DML_e_DQL_.sql
@@ -1,9 +1,9 @@
+use ExercicioDeAgregação
+
+
 insert into Produtos(DESCRICAO, Preco)
 values ('Teclado', 55), ('Mouse', 30), ('Fone de ouvido', 10), ('Mousepad', 15)
 
-select * from Produtos
 
+select SUM (Preco) from Produtos       --Colocaria a Função nativa depois do Select, e no "()" colocaria a coluna que quer usar a função
 
-select (Preco) from Produtos
-
-drop database HROADSFINAL
